f9cfcbbaf0689cd85847f7cd035e56d160654200
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-01-23 00:32:29 +0100

    styled the header links a bit more.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,55 @@
 import React from "react";
+
+// tools
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+
+// styles
+import "./Header.css";
 
 const Header = () => {
 	return (
 		<div className="header">
 			<div className="header__logo">
-				<Link to={"/"}>LOGO</Link>
+				<Link className="link" to={"/"}>
+					LOGO
+				</Link>
 			</div>
 			<div className="header__navigation">
-				<HashLink to="/#aboutMe">About me</HashLink>
-				<HashLink to="/#projects">Projects</HashLink>
-				<HashLink to="/#testimonials">
-					<p>Testimonials</p>
-				</HashLink>
-				<HashLink to="/#contact">Contact</HashLink>
-				<a
-					target="_blank"
-					rel="noreferrer"
-					href="https://github.com/DariuszLegizynski/myPortfolio"
-					download
-				>
-					Resume
-				</a>
+				<div className="header__navigation__item">
+					<HashLink className="link" to="/#aboutMe">
+						About me
+					</HashLink>
+				</div>
+				<div className="header__navigation__item">
+					<HashLink className="link" to="/#projects">
+						Projects
+					</HashLink>
+				</div>
+				<div className="header__navigation__item">
+					<HashLink
+						className="link"
+						to="/#testimonials"
+					>
+						Testimonials
+					</HashLink>
+				</div>
+				<div className="header__navigation__item">
+					<HashLink className="link" to="/#contact">
+						Contact
+					</HashLink>
+				</div>
+				<div className="header__navigation__item">
+					<a
+						className="link"
+						target="_blank"
+						rel="noreferrer"
+						href="https://github.com/DariuszLegizynski/myPortfolio"
+						download
+					>
+						Resume
+					</a>
+				</div>
 			</div>
 		</div>
 	);
