6b6c66e7a5d1dd2a1749d87e7155a6ff82997186
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-01-26 23:50:32 +0100

    finished styling my testimonials section. Made custom dots and added links (not working yet) to the testimonials.

--- src/components/testimonial/Testimonial.js
+++ src/components/testimonial/Testimonial.js
@@ -1,10 +1,21 @@
 import React from "react";
 
+// styles
+import "./Testimonial.css";
+
 const Testimonial = ({ description, title }) => {
 	return (
-		<div className="testimonials__text">
+		<div className="testimonial__text">
 			<p>{description}</p>
 			<h4>{title}</h4>
+			<a
+				className="link"
+				target="_blank"
+				rel="noreferrer"
+				href="https://github.com/DariuszLegizynski"
+			>
+				link
+			</a>
 		</div>
 	);
 };
